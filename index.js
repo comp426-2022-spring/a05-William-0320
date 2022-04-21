d38af23ca3945b020b689ba24b8f04fc26140d96
William-0320 <lisiyuan010320@hotmail.com>
2022-04-21 15:13:04 -0400

    Included dependencies in index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Place your server entry point code here
+// Require all the dependencies used
+const fs = require('fs');
+const morgan = require('morgan');
+const express = require('express');
+const cors = require('cors');
+const minimist = require('minimist');
+const app = express();
+const args = require('minimist')(process.argv.slice(2));
+
