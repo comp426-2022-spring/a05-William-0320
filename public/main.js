6c66f3427acd1e21e04cd20296fca577ae226429
William-0320 <lisiyuan010320@hotmail.com>
2022-04-26 16:18:34 -0400

    Added nav button click skeletons

--- public/main.js
+++ public/main.js
@@ -1,5 +1,19 @@
 // Focus div based on nav button click
+document.getElementById("homenav").onclick = function(){
 
+}
+
+document.getElementById("singlenav").onclick = function(){
+
+}
+
+document.getElementById("multinav").onclick = function(){
+
+}
+
+document.getElementById("guessnav").onclick = function(){
+    
+}
 // Flip one coin and show coin image to match result when button clicked
 
 // Flip multiple coins and show coin images in table as well as summary results
