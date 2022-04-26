646fd67885e1a73f91d5c5eb7866b7bc9ca87dae
William-0320 <lisiyuan010320@hotmail.com>
2022-04-26 14:59:09 -0400

    Added endpoint for guessing a coin flip

--- index.js
+++ index.js
@@ -131,4 +131,9 @@ app.post('/app/flip/coins/', (req, res, next) => {
     const flips = coinFlips(req.body.number)
     const count = countFlips(flips)
     res.status(200).json({"raw":flips,"summary":count})
-})
+});
+
+app.post('/app/flip/call/', (req, res, next) => {
+    const game = flipACoin(req.body.guess)
+    res.status(200).json(game)
+});
